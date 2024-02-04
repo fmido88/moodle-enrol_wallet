03d6ab5a2b3167df2c05aa726626b3aadaf51299
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2024-02-04 21:44:16 +0200

    v inc

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024020400;
+$plugin->version   = 2024020421;
 $plugin->requires  = 2020061500;
 $plugin->component = 'enrol_wallet';
-$plugin->release   = '5.0.1';
+$plugin->release   = '5.0.2';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
     'enrol_manual' => ANY_VERSION,
