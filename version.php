307b8f40d939ed22dac955d8d8d694e67d521821
Mo Farouk <122031508+fmido88@users.noreply.github.com>
2024-06-16 14:17:28 +0300

    increase version

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2024030900;
+$plugin->version   = 2024061600;
 $plugin->requires  = 2020061500;
 $plugin->component = 'enrol_wallet';
-$plugin->release   = '5.3.2';
+$plugin->release   = '5.4.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->dependencies = [
     'enrol_manual' => ANY_VERSION,
